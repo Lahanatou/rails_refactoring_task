5d5e374e27c7181f4f971298c11961cdf329f603
lahanatou <nouryous30@gmail.com>
2023-01-13 21:22:42 +0100

    commit

--- app/models/club.rb
+++ app/models/club.rb
@@ -40,9 +40,9 @@ class Club < ApplicationRecord
     count_result_on(year, "draw")
   end
 
-  def homebase
-    "#{hometown}, #{country}"
-  end
+  # def homebase
+  #   "#{hometown}, #{country}"
+  # end
 
   def average_age
     (self.players.sum(&:age) / self.players.length).to_f
